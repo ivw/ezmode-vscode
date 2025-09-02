b24a1b0f69d72db2c2771cf0e45b7590e7de172d
Ivo <ivowijnker@hotmail.com>
2025-09-02 13:02:44 -0400

    add recommended linting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,36 +1,6 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin"
-import tsParser from "@typescript-eslint/parser"
-import eslintConfigPrettier from "eslint-config-prettier/flat"
+// @ts-check
 
-export default [
-  {
-    files: ["**/*.ts"],
-  },
-  {
-    plugins: {
-      "@typescript-eslint": typescriptEslint,
-    },
+import { defineConfig } from "eslint/config"
+import tseslint from "typescript-eslint"
 
-    languageOptions: {
-      parser: tsParser,
-      ecmaVersion: 2022,
-      sourceType: "module",
-    },
-
-    rules: {
-      "@typescript-eslint/naming-convention": [
-        "warn",
-        {
-          selector: "import",
-          format: ["camelCase", "PascalCase"],
-        },
-      ],
-
-      curly: "warn",
-      eqeqeq: "warn",
-      "no-throw-literal": "warn",
-      semi: "warn",
-    },
-  },
-  eslintConfigPrettier,
-]
+export default defineConfig(tseslint.configs.recommended)
