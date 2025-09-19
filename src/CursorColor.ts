f74d9113cbff6e5bfbe4c4fb4f969239d205fab6
Ivo <ivowijnker@hotmail.com>
2025-09-19 12:45:33 -0400

    clean CursorColor code

--- src/CursorColor.ts
+++ src/CursorColor.ts
@@ -1,15 +1,7 @@
 import * as vscode from "vscode"
 
-// TODO handle in global state. Another window might have changed it already.
-let originalCursorColor: string | undefined | null = null
-
 export function changeCursorColor(color: string | undefined) {
   const configuration = vscode.workspace.getConfiguration("workbench")
-
-  if (originalCursorColor === null) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    originalCursorColor = configuration.get<any>("colorCustomizations")?.["editorCursor.foreground"]
-  }
 
   // Set the color in the global user settings.
   configuration.update("colorCustomizations", { "editorCursor.foreground": color }, true)
@@ -17,5 +9,4 @@ export function changeCursorColor(color: string | undefined) {
 
 export function resetCursorColor() {
   changeCursorColor(undefined)
-  // changeCursorColor(originalCursorColor === null ? undefined : originalCursorColor)
 }
