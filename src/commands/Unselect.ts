af60467bc4d11d4c5b5558db9859ff4f511a5d64
Ivo <ivowijnker@hotmail.com>
2025-10-07 14:42:04 -0400

    reformat and use LF line endings only

--- src/commands/Unselect.ts
+++ src/commands/Unselect.ts
@@ -4,13 +4,8 @@ import * as vscode from "vscode"
  * `ezmode.unselect` is different from `cancelSelection` because it keeps all cursors.
  */
 export function activateUnselect(context: vscode.ExtensionContext) {
-  const disposable = vscode.commands.registerTextEditorCommand(
-    "ezmode.unselect",
-    (editor) => {
-      editor.selections = editor.selections.map((sel) => {
-        return new vscode.Selection(sel.active, sel.active)
-      })
-    },
-  )
+  const disposable = vscode.commands.registerTextEditorCommand("ezmode.unselect", (editor) => {
+    editor.selections = editor.selections.map((sel) => new vscode.Selection(sel.active, sel.active))
+  })
   context.subscriptions.push(disposable)
 }
