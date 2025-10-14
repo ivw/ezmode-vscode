f082a48e4ed7fabfc7655f1acc02d361fe2fef15
Ivo <ivowijnker@hotmail.com>
2025-10-14 16:46:49 -0400

    allow custom menu items

--- src/commands/ShowEzModeMenu.ts
+++ src/commands/ShowEzModeMenu.ts
@@ -1,21 +1,20 @@
 import * as vscode from "vscode"
 import { getCommandTitle, registerCommand } from "../utils/Commands"
 
-const menuCommands: Array<string> = [
-  "ezmode.editEzModeRc",
-  "ezmode.reloadEzModeRc",
-  "ezmode.cheatsheet.focus",
-]
-
 type CommandQuickPickItem = vscode.QuickPickItem & { commandId: string }
+
+function createCommandQuickPickItem(commandId: string): CommandQuickPickItem {
+  return { commandId, label: getCommandTitle(commandId) }
+}
+
+const menuItems: Array<CommandQuickPickItem> = [
+  createCommandQuickPickItem("ezmode.editEzModeRc"),
+  createCommandQuickPickItem("ezmode.reloadEzModeRc"),
+  { label: "Toggle EzMode cheat sheet", commandId: "ezmode.cheatsheet.focus" },
+]
 
 export function activateOpenEzModeMenu(context: vscode.ExtensionContext) {
   registerCommand(context, "ezmode.openEzModeMenu", async () => {
-    const menuItems: Array<CommandQuickPickItem> = menuCommands.map((menuCommand) => ({
-      commandId: menuCommand,
-      label: getCommandTitle(menuCommand),
-    }))
-
     const pickedItem = await vscode.window.showQuickPick(menuItems, {})
     if (pickedItem) {
       vscode.commands.executeCommand(pickedItem.commandId)
