d707d8ad55265fb58b49450f7ed3b9a6f456baf0
Ivo <ivowijnker@hotmail.com>
2025-12-08 18:59:39 -0500

    ezmode.executeCommand: Add optional parameter to execute a specific command

--- src/commands/ExecuteEzCommand.ts
+++ src/commands/ExecuteEzCommand.ts
@@ -4,14 +4,16 @@ import { performSingleAction } from "../config/EnvState"
 import { parseLine } from "../config/Parser"
 
 export function activateExecuteEzCommand(context: vscode.ExtensionContext) {
-  registerCommand(context, "ezmode.executeCommand", async () => {
-    const result = await vscode.window.showInputBox({
-      title: "Execute EzMode Command",
-      placeHolder: "mode ez",
-    })
-    if (result) {
+  registerCommand(context, "ezmode.executeCommand", async (args) => {
+    const commandStr: string =
+      args?.command ||
+      (await vscode.window.showInputBox({
+        title: "Execute EzMode Command",
+        placeHolder: "mode ez",
+      }))
+    if (commandStr) {
       try {
-        const action = parseLine(result)
+        const action = parseLine(commandStr)
         if (action) {
           performSingleAction(action)
         }
