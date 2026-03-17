032c9f042eb99b035f5ce252df67ae36f5daef2d
Ivo <ivowijnker@hotmail.com>
2026-03-17 11:15:56 -0400

    handle replacePreviousChar, fixing a bug that happens with certain key combinations outside type mode

--- src/config/ModeConfig.ts
+++ src/config/ModeConfig.ts
@@ -2,6 +2,7 @@ import * as vscode from "vscode"
 import { isLoadingConfig } from "./LoadConfig"
 import { getMode } from "../mode/ModeState"
 import { registerCommand } from "../utils/Commands"
+import { nativeEzAction } from "./EzActions"
 
 export const DEFAULT_KEY = "default"
 
@@ -46,17 +47,21 @@ export function addBinding(mode: string, keyBinding: KeyBinding) {
   addBindingToModeConfig(modeConfig, keyBinding)
 }
 
+export function getActionBasedOnMode(key: string, mode: string = getMode()): EzAction | null {
+  const modeConfig = getModeConfig(mode)
+  if (!modeConfig) return null
+
+  const keyBinding = modeConfig.keyBindings.get(key) ?? modeConfig.keyBindings.get(DEFAULT_KEY)
+  if (!keyBinding) return null
+
+  return keyBinding.action
+}
+
 export function handleKeyBasedOnMode(
   key: string,
   mode: string = getMode(),
 ): Thenable<unknown> | void {
-  const modeConfig = getModeConfig(mode)
-  if (!modeConfig) return
-
-  const keyBinding = modeConfig.keyBindings.get(key) ?? modeConfig.keyBindings.get(DEFAULT_KEY)
-  if (!keyBinding) return
-
-  return keyBinding.action(key)
+  return getActionBasedOnMode(key, mode)?.(key)
 }
 
 export function activateModalTypeHandler(context: vscode.ExtensionContext) {
@@ -64,5 +69,18 @@ export function activateModalTypeHandler(context: vscode.ExtensionContext) {
   registerCommand(context, "type", (args) => {
     const key = args.text as string
     handleKeyBasedOnMode(key)
+  })
+
+  registerCommand(context, "replacePreviousChar", (args) => {
+    if (typeof args?.text !== "string" || args.text.length === 0) return
+    const key: string = args.text[0]
+    const action = getActionBasedOnMode(key)
+    if (action) {
+      if (action === nativeEzAction) {
+        vscode.commands.executeCommand("default:replacePreviousChar", args)
+      } else {
+        action(key)
+      }
+    }
   })
 }
